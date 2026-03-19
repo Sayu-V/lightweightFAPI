ea4081c65bd4bf7d47fb9f5faba3d53f2c62e789
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-19 19:50:23 +0530

    Update main.py
    Add Expense API (POST /expense)
    🎯 Goal
    
    Accept expense data
    
    Validate it
    
    Store it
    
    Return response

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
-from models import Income
+from models import Income, Expense
 
 app = FastAPI()
 
 # In-memory storage
 incomes = []
+expenses = []   # ✅ NEW
 
 @app.get("/")
 def home():
     return {"message": "Finance API is running 🚀"}
 
-# ✅ NEW ENDPOINT
+# Income API
 @app.post("/income")
 def add_income(data: Income):
     incomes.append(data.amount)
@@ -19,4 +20,15 @@ def add_income(data: Income):
         "message": "Income added successfully",
         "amount": data.amount,
         "source": data.source
+    }
+
+# ✅ NEW Expense API
+@app.post("/expense")
+def add_expense(data: Expense):
+    expenses.append(data.amount)
+
+    return {
+        "message": "Expense added successfully",
+        "amount": data.amount,
+        "category": data.category
     }
