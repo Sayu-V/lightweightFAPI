6acb4497694ef76f39fd52beca707069fadd4550
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-19 19:51:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
-from models import Income, Expense
+from models import Income
 
 app = FastAPI()
 
 # In-memory storage
 incomes = []
-expenses = []   # ✅ NEW
 
 @app.get("/")
 def home():
     return {"message": "Finance API is running 🚀"}
 
-# Income API
+# ✅ NEW ENDPOINT
 @app.post("/income")
 def add_income(data: Income):
     incomes.append(data.amount)
@@ -20,15 +19,4 @@ def add_income(data: Income):
         "message": "Income added successfully",
         "amount": data.amount,
         "source": data.source
-    }
-
-# ✅ NEW Expense API
-@app.post("/expense")
-def add_expense(data: Expense):
-    expenses.append(data.amount)
-
-    return {
-        "message": "Expense added successfully",
-        "amount": data.amount,
-        "category": data.category
     }
