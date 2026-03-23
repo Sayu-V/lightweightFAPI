c156828aa814442e8a259ec3667a10214b61bcf8
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-23 08:48:17 +0530

    Update expense.py

--- routes/expense.py
+++ routes/expense.py
@@ -1,1 +1,14 @@
+from fastapi import APIRouter, HTTPException
+from app.models import Expense, MessageResponse
 
+router = APIRouter()
+
+expenses = []
+
+@router.post("/expense", response_model=MessageResponse, tags=["Finance"])
+def add_expense(data: Expense):
+    if data.amount <= 0:
+        raise HTTPException(status_code=400, detail="Amount must be greater than 0")
+
+    expenses.append(data.amount)
+    return {"message": "Expense added successfully"}
