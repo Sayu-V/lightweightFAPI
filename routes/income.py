2facf77373e07aab3fba79af035662992049d452
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-23 08:47:29 +0530

    Update income.py

--- routes/income.py
+++ routes/income.py
@@ -1,1 +1,15 @@
 
+from fastapi import APIRouter, HTTPException
+from app.models import Income, MessageResponse
+
+router = APIRouter()
+
+incomes = []
+
+@router.post("/income", response_model=MessageResponse, tags=["Finance"])
+def add_income(data: Income):
+    if data.amount <= 0:
+        raise HTTPException(status_code=400, detail="Amount must be greater than 0")
+
+    incomes.append(data.amount)
+    return {"message": "Income added successfully"}
