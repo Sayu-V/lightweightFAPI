164d8b198396051e98abffe313a790e60ca92c2b
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-23 08:49:00 +0530

    Update summary.py

--- routes/summary.py
+++ routes/summary.py
@@ -1,1 +1,14 @@
+from fastapi import APIRouter
+from app.models import SummaryResponse
+from app.routes.income import incomes
+from app.routes.expense import expenses
 
+router = APIRouter()
+
+@router.get("/summary", response_model=SummaryResponse, tags=["Finance"])
+def get_summary():
+    return {
+        "total_income": sum(incomes),
+        "total_expense": sum(expenses),
+        "balance": sum(incomes) - sum(expenses)
+    }
